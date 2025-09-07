581f0d6da807edbb90b98fedb9507fb1cefdfb2a
Amar Hazem <amar.hazem@icloud.com>
2025-09-07 21:12:55 +0200

    chore(portfolio): add manifest.ts file (#38)

---
+++ apps/portfolio/src/app/(frontend)/manifest.ts
@@ -0,0 +1,20 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    background_color: "#000000",
+    description: "Amar Hazem | Full Stack Developer",
+    display: "standalone",
+    icons: [
+      {
+        sizes: "any",
+        src: "/favicon.ico",
+        type: "image/x-icon",
+      },
+    ],
+    name: "Amar Hazem | Full Stack Developer",
+    short_name: "Amar Hazem",
+    start_url: "/",
+    theme_color: "#D21DF7",
+  };
+}
