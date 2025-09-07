0bea81c464a6a1fb5a66e01790564663d88dc0ce
Amar Hazem <amar.hazem@icloud.com>
2025-09-07 16:01:29 +0200

    chore(portfolio): install @mui/material (#12)

--- apps/portfolio/src/app/(frontend)/page.tsx
+++ apps/portfolio/src/app/(frontend)/page.tsx
@@ -1,5 +1,16 @@
+import Typography from "@mui/material/Typography";
 import { type ReactNode } from "react";
 
 export default async function LandingPage(): Promise<ReactNode> {
-  return <h1>Amar Hazem | Full Stack Developer</h1>;
+  return (
+    <Typography
+      sx={{
+        alignSelf: "center",
+        textAlign: "center",
+      }}
+      variant="h2"
+    >
+      Amar Hazem | Full Stack Developer
+    </Typography>
+  );
 }
