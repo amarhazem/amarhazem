8c1e771759645a2596c199c6059d3a32675de66d
Amar Hazem <amar.hazem@icloud.com>
2025-09-07 14:56:06 +0200

    chore(portfolio): install @vercel/analytics (#11)

--- apps/portfolio/src/app/(frontend)/layout.tsx
+++ apps/portfolio/src/app/(frontend)/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { type ReactNode } from "react";
 
@@ -16,6 +17,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Analytics />
         <SpeedInsights />
         <main>{children}</main>
       </body>
